e1a3b9ca4597ca17634a204275436fd61cabe722
David <kabathi98@gmail.com>
2025-10-28 12:20:11 +0300

    Add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+
 function App() {
-  return <div>MenuMate</div>;
+  return (
+    <div>menumate</div>
+  );
 }
 export default App;
