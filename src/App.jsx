c124dc2617e44a44651cc1814ff1ffb95d49079f
David <kabathi98@gmail.com>
2025-10-28 15:58:03 +0300

    style dishcard content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "./pages/Menu";
-
+import DishCard from "./components/DishCard";
 function App() {
   return (
-    <div>menumate</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </div>
   );
 }
 export default App;
