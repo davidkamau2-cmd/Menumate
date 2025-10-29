7459d44d40db39dafec5930c3460fd35a8a2296a
David <kabathi98@gmail.com>
2025-10-29 09:37:41 +0300

    add app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Menu from "./pages/Menu";
-import DishCard from "./components/DishCard";
+import Cart from "./components/Cart";
+import Favourites from "./components/Favourites";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-    </div>
+    </>
   );
 }
+
 export default App;
