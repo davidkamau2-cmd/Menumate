122e305252a7fc22c605d2685a49bb957bd0f213
David <kabathi98@gmail.com>
2025-10-31 03:57:34 +0300

    fix signup problem

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -23,13 +23,13 @@ function Signup() {
       return;
     }
 
-    try {await signup(email, password);
-      await signup(email, password);
-      navigate("/menu");
-    } catch (err) {
-      setError("Error creating account");
-    }
-  };
+  try {
+    await signup(email, password);
+    navigate("/menu");
+  } catch (err) {
+    setError("Error creating account. Try again.");
+  }
+};
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
